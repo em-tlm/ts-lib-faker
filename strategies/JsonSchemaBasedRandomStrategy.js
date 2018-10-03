2b46f677a2a3ab076740d97f96fc351edcad464a
Milos Grbic <mgrbic@tetrascience.com>
2018-10-03 08:27:14 +0200

    Change dates to ISO format

--- strategies/JsonSchemaBasedRandomStrategy.js
+++ strategies/JsonSchemaBasedRandomStrategy.js
@@ -17,14 +17,11 @@ class JsonSchemaBasedRandomStrategy extends Strategy {
         this.dataCount = (options.dataCount) ? options.dataCount : 10;
 
         this.validate = ajv.compile(this.jsonSchema);
+        this.extendFieldFakerApi();
+        this.extendFieldChanceApi();
     }
 
     async generateValue(tsLibFaker) {
-        // Wrapper around faker.js library that contains
-        // various algorithms for random generation.
-        // Not to be confused with ts-lib-faker instance
-        jsf.extend('faker', () => require('faker'));
-        jsf.extend('chance', () => new Chance());
         const resultArray = _.map(_.range(this.dataCount), () => jsf.resolve(this.jsonSchema));
         const objectArray = await Promise.all(resultArray);
         const value = objectArray.map(x => JSON.stringify(x));
@@ -32,6 +29,33 @@ class JsonSchemaBasedRandomStrategy extends Strategy {
             count: tsLibFaker._counter,
             value
         }
+    }
+
+    extendFieldFakerApi() {
+        // Wrapper around faker.js library that contains
+        // various algorithms for random generation.
+        // Not to be confused with ts-lib-faker instance
+        jsf.extend('faker', () => {
+            const faker = require('faker');
+            const dateIsoWrapper = object => {
+                return new Proxy(object, {
+                    get(target, propKey) {
+                        let targetMethod = target[propKey];
+                        if (typeof targetMethod === "function") {
+                            return targetMethod().toISOString();
+                        }
+                        return targetMethod;
+                    }
+                })
+            };
+            faker.dateISO = dateIsoWrapper(faker.date);
+
+            return faker;
+        });
+    }
+
+    extendFieldChanceApi() {
+        jsf.extend('chance', () => new Chance());
     }
 
 }
